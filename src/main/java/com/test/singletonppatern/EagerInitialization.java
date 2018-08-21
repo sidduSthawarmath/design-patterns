f2be50d595317bd1d95bd078d9a57feb43ff233f
Siddu <siddusthawarmath@gmail.com>
2018-08-21 18:55:57 +0530

    singleton design pattern - eager initialization

---
+++ src/main/java/com/test/singletonppatern/EagerInitialization.java
@@ -0,0 +1,24 @@
+package com.test.singletonppatern;
+
+/*
+ * In eager initialization, the instance of Singleton Class is created at the
+ * time of class loading, this is the easiest method to create a singleton class
+ * but it has a drawback that instance is created even though client application
+ * might not be using it.
+ */
+
+public class EagerInitialization {
+
+	private EagerInitialization() {
+	}
+
+	private static final EagerInitialization INSTANCE = new EagerInitialization();
+
+	public static EagerInitialization getInstance() {
+		return INSTANCE;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(EagerInitialization.getInstance());
+	}
+}
