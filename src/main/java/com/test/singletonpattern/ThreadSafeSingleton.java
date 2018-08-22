d6633c9934de7b3fc00e76cc68217a40a61d5050
Siddu <siddusthawarmath@gmail.com>
2018-08-22 10:52:50 +0530

    Thread safe singleton

--- src/main/java/com/test/singletonpattern/ThreadSafeSingleton.java
+++ src/main/java/com/test/singletonpattern/ThreadSafeSingleton.java
@@ -4,10 +4,26 @@ public class ThreadSafeSingleton {
 
 	private ThreadSafeSingleton(){}
 	
+/*	
+	 * The EagerInitialization,LazyInitialization,StaticBlockInitialization singleton 
+	 * implementation works fine in case of single threaded environment
+	 * but when it comes to multithreaded systems, it can cause issues if
+	 * multiple threads are inside the if loop at the same time. It will destroy
+	 * the singleton pattern and both threads will get the different instances
+	 * of singleton class.
+	 */
 	public static ThreadSafeSingleton instance=null;
-	
-	
-	
+
+	public static synchronized ThreadSafeSingleton getInstance() {
+		if (instance == null) {
+			instance = new ThreadSafeSingleton();
+		}
+		return instance;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Instance:\t" + ThreadSafeSingleton.getInstance());
+	}
 	
 	
 }
