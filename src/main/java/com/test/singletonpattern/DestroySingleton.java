1a2a447379301eaa9749f6217a7286fbfba4352b
Siddu <siddusthawarmath@gmail.com>
2018-08-22 14:27:11 +0530

    Destroy singleton using Reflection

---
+++ src/main/java/com/test/singletonpattern/DestroySingleton.java
@@ -0,0 +1,35 @@
+package com.test.singletonpattern;
+
+import java.lang.reflect.Constructor;
+
+/*
+ * 7.When you run the above test class, you will notice that hashCode of
+ * both the instances are not same that destroys the singleton pattern.
+ * Reflection is very powerful and used in a lot of frameworks like Spring
+ * and Hibernate
+ */
+
+public class DestroySingleton {
+
+	public static void main(String[] args) {
+
+		EagerInitialization instanceOne = EagerInitialization.getInstance();
+		EagerInitialization instanceTwo = null;
+		try {
+			Constructor[] constructors = EagerInitialization.class.getDeclaredConstructors();
+			for (Constructor constructor : constructors) {
+				// Below code will destroy the singleton pattern
+				constructor.setAccessible(true);
+				instanceTwo = (EagerInitialization) constructor.newInstance();
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("Singleton instanceOne hashcode:\t" + instanceOne.hashCode());
+		System.out.println("Singleton instanceTwo hashcode:\t" + instanceTwo.hashCode());
+
+	}
+
+}
