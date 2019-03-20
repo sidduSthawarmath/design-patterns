59519960ffffe48a9caa6fbbd19a67d7ceee6349
Siddu <siddusthawarmath@gmail.com>
2019-03-20 14:55:15 +0530

    prototype design pattern

--- src/main/java/com/siddu/prototype/pattern/MainPrototype.java
+++ src/main/java/com/siddu/prototype/pattern/MainPrototype.java
@@ -5,21 +5,21 @@ import java.util.List;
 public class MainPrototype {
 	public static void main(String[] args) throws CloneNotSupportedException {
 		Employees emp=new Employees();
-		emp.loadData();
+		emp.loadData(); //List created with [Siddu, Mahesh, Ganesh, Raghav]
 		
-		Employees emp1 = (Employees) emp.clone();
+		Employees emp1 = (Employees) emp.clone(); // data cloned getting new list
 		List<String> empList1 = emp1.getEmpList();
 		
 		
-		Employees emp2 = (Employees) emp.clone();
+		Employees emp2 = (Employees) emp.clone(); // data cloned getting new list
 		List<String> empList2 = emp2.getEmpList();
 		
-		empList1.add("Suresh");
-		System.out.println(empList1);
+		empList1.add("Suresh"); //added new data to cloned list
+		System.out.println(empList1); //[Siddu, Mahesh, Ganesh, Raghav, Suresh]
 		
 		
-		empList2.remove("Siddu");
-		System.out.println(empList2);
+		empList2.remove("Siddu");//removed existing data to cloned list
+		System.out.println(empList2);  //[Mahesh, Ganesh, Raghav]
 
 
 	}
